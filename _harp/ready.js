f0c8c8d01c2a686eba729e6b725ed36bc8668dc0
Rich von Rauchhaupt <Rich@Snow.laptop>
2017-10-14 11:07:00 -0400

    updated site content, removed liweb branding

--- _harp/ready.js
+++ _harp/ready.js
@@ -3,7 +3,24 @@ $(document).ready(function () {
   // page is now ready, initialize the calendar...
 
   $('#calendar').fullCalendar({
-    // put your options and callbacks here
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'listMonth,month'
+    },
+  
+   eventSources: [
+
+      // your event source
+      {
+          url: 'https://api.liweb.group/events/fullcal', // use the `url` property
+          
+      }
+
+      // any other sources...
+
+  ]
+
   })
 
 });
